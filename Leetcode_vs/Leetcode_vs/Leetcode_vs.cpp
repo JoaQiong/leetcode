3738f43ce916db6cc64c77eabd863b36ee67caf8
Joa <duanqiongqiong@ict.ac.cn>
2017-04-04 13:10:56 +0800

    add leetcode vs

---
+++ Leetcode_vs/Leetcode_vs/Leetcode_vs.cpp
@@ -0,0 +1,16 @@
+// Leetcode_vs.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include "math.h"
+using namespace std;
+int main()
+{
+	//printf("%d",13);
+	cout<<fmax(1, 3);
+	getchar();
+    return 0;
+	
+}
+
